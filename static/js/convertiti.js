72772aa623a4f0ca351179d9a031c5d8698bf210
o-crii <cristianpesola@gmail.com>
2023-02-23 09:56:18 +0100

    sistemate alcune cose

--- static/js/convertiti.js
+++ static/js/convertiti.js
@@ -3,17 +3,19 @@ function AssNumero() {
   let bin = "";
 
   function convertiti(n) {
-    if(n==0) {
+    if(n===0) {
 
       return bin;
+      //return alert("Ricarica pagina perché il numero inserito non mi piace")
     } else {
 
-      bin = (n % 2) + bin;
+      bin=(n%2)+bin;
 
       return convertiti((n-(n%2))/2);
     }
   }
 
   let NumConv = document.getElementById("NumConv");
+
   NumConv.innerHTML = "Il numero convertito in binario è: " + convertiti(Num);
 }
